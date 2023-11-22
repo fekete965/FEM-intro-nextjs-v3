3454506893318edf3cbcb841baf729cd0f046139
Bence Fekete <fekete965@gmail.com>
2023-11-22 00:05:19 +0000

    Add a loader to new-user page

---
+++ app/(auth)/new-user/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from '@/components/Loader'
+
+const NewUserLoader = () => {
+  return (
+    <section className="flex justify-center items-center bg-slate-900 w-screen h-screen">
+      <Loader />
+    </section>
+  )
+}
+
+export default NewUserLoader
