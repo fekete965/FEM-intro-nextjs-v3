e61ea45ebc86c960ad1af609df7cc40eb71c581b
Bence Fekete <fekete965@gmail.com>
2023-11-24 09:11:35 +0000

    Save or create new analysis on journal content update

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+import { analyze } from '@/utils/ai'
 import { getUserByClerkId } from '@/utils/auth'
 import prisma from '@/utils/db'
 
@@ -27,6 +28,42 @@ export const PATCH = async (req: NextRequest, searchParams: SearchParams) => {
       content: body.content,
     },
   })
+
+  const analysisData = await analyze(updatedEntry.content)
+
+  const journalAnalysis = await prisma.analysis.findUnique({
+    where: {
+      journalEntryId: updatedEntry.id,
+    },
+  })
+
+  if (journalAnalysis) {
+    await prisma.analysis.update({
+      data: {
+        color: analysisData.color,
+        isNegative: analysisData.isNegative,
+        mood: analysisData.mood,
+        sentimentScore: analysisData.sentimentScore,
+        subject: analysisData.subject,
+        summary: analysisData.summary,
+      },
+      where: {
+        id: journalAnalysis.id,
+      },
+    })
+  } else {
+    await prisma.analysis.create({
+      data: {
+        color: analysisData.color,
+        isNegative: analysisData.isNegative,
+        journalEntryId: updatedEntry.id,
+        mood: analysisData.mood,
+        sentimentScore: analysisData.sentimentScore,
+        subject: analysisData.subject,
+        summary: analysisData.summary,
+      },
+    })
+  }
 
   return NextResponse.json({ body: updatedEntry })
 }
