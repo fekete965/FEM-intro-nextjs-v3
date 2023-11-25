eb5ad69ffc20ef96f9c58b1939f37e5fb8945da7
Bence Fekete <fekete965@gmail.com>
2023-11-25 14:58:41 +0000

    Update dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { UserButton, UserProfile } from '@clerk/nextjs'
+import Link from 'next/link'
 
 import { ReactNode } from 'react'
 
@@ -6,14 +7,30 @@ type Props = {
   children: ReactNode
 }
 
+const navLinks = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+  { href: '/history', label: 'History' },
+]
+
 const DashboardLayout = ({ children }: Props) => {
   return (
     <main className="grid grid-cols-[repeat(8,_minmax(auto,_1fr))] grid-rows-[repeat(9,_minmax(auto,_1fr))] w-full h-screen bg-slate-900 text-white">
-      <aside className="h-full text-center border-r p-4 col-span-1 row-span-full border-gray-500">Mood</aside>
+      <aside className="h-full text-center border-r p-4 col-span-1 row-span-full border-gray-500">
+        <div>Mood Logo</div>
+        <nav>
+          <ul>
+            {navLinks.map(link => (
+              <li key={link.label}>
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </aside>
       <header className="flex justify-end items-center py-4 px-8 col-span-7 row-span-1 border-b border-gray-500">
         <UserButton />
       </header>
-      <h2 className="flex justify-center items-center text-3xl col-span-7 row-span-1">Journal</h2>
       <section className="p-4 col-span-7 row-[span_7_/_span_7]">
         <div className="h-full overflow-auto">{children}</div>
       </section>
